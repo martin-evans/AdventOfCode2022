311c2de2c69618d5dfc123e8550a8117ea3d0360
Martin Evans <martin.evans@pebblepad.com>
2022-12-02 09:59:52 +0000

    Added breaking test

--- Day1/MJE.Advent.CalorieCounting.Tests.Unit/UnitTest1.cs
+++ Day1/MJE.Advent.CalorieCounting.Tests.Unit/UnitTest1.cs
@@ -1,15 +1,59 @@
+using NUnit.Framework.Constraints;
+
 namespace MJE.Advent.CalorieCounting.Tests.Unit;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+
+
+    (int, int) RunTest()
     {
+        
+        var calorieList = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        return CalorieQueryRunner.Run(calorieList);
+        
     }
 
+
     [Test]
-    public void Test1()
+    public void ReturnCorrectElfIndex()
     {
-        Assert.Pass();
+        
+        (int elfIndex, _ ) = RunTest();
+        
+        Assert.AreEqual(4,elfIndex);
+
+    }
+
+    // [Test]
+    // public void ReturnCorrectCalorieCount()
+    // {
+    //     
+    //     (_, int totalCalories ) = RunTest();
+    //     
+    //     Assert.AreEqual(2400,totalCalories);
+    //     
+    // }
+}
+
+public class CalorieQueryRunner
+{
+    public static (int elfIndex, int totalCalories) Run(string calorieList)
+    {
+        return (0, 0);
     }
 }
