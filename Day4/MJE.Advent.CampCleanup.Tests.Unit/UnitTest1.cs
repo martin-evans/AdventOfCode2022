3230acca3e7af2f83aa98ef0852c0d97dbc73302
Martin Evans <martin.evans@mailfence.com>
2022-12-04 13:15:50 +0000

    Initial add

---
+++ Day4/MJE.Advent.CampCleanup.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MJE.Advent.CampCleanup.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
