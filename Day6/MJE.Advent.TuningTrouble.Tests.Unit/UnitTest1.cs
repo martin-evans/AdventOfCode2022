196a32074b34cf11468baa76b5165df48cd549e8
Martin Evans <martin.evans@mailfence.com>
2022-12-06 07:17:45 +0000

    Initial add

---
+++ Day6/MJE.Advent.TuningTrouble.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MJE.Advent.TuningTrouble.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
